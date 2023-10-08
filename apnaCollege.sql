b6c0eac23f82db3ba670010b3b6e1c1900020474
Tanmay Upreti <iamtanmayupreti@gmail.com>
2023-10-08 16:40:06 +0530

    DB Related Queries

--- apnaCollege.sql
+++ apnaCollege.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE School;
-
+CREATE DATABASE IF NOT EXISTS School;
 USE School;
 
 CREATE TABLE Student(
@@ -10,6 +10,29 @@ CREATE TABLE Student(
 
 INSERT INTO Student VALUES (1, "Aayushi", 16);
 INSERT INTO Student VALUES (2, "Aditi", 14);
--- INSERT INTO Student VALUES (1, "Aditi", 14);
+-- INSERT INTO Student VALUES (1, "Aditi", 14); gives error b/c id is special
 
 SELECT * FROM Student;
+INSERT INTO Student VALUES (39, "Tanmay", 15);
+SELECT * FROM Student;
+
+CREATE DATABASE IF NOT EXISTS College;
+DROP DATABASE IF EXISTS Collage;
+DROP DATABASE Collage;
+DROP DATABASE IF EXISTS College;
+
+SHOW DATABASES;
+
+USE School;
+SHOW TABLES;
+
+
+
+
+
+
+
+
+
+
+
